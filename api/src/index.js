2d66382ac45632ab3430485bf7b3cc7dcfb4cb78
Apolo <mi.ohta5@gmail.com>
2021-09-09 14:59:41 -0300

    mtr

---
+++ api/src/index.js
@@ -0,0 +1,66 @@
+import db from './db.js';
+import express from 'express'
+import cors from 'cors'
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+app.get('/matricula', async (req, resp) => {
+    try {
+        let alunos = await db.tb_matricula.findAll({ order: [['id_matricula', 'desc']] });
+        resp.send(alunos);
+    } catch (error) {
+        resp.send({ erro: error.toString() })
+    }
+})
+
+app.post('/matricula', async (req, resp) => {
+    try {
+        let { nome, chamada, curso, turma } = req.body;
+
+        let r = await db.tb_matricula.create({
+            nm_aluno: nome,
+            nr_chamada: chamada,
+            nm_curso: curso,
+            nm_turma: turma
+        })
+        resp.send(r)
+    } catch (error) {
+        resp.send({ erro: error.toString() })
+    }
+})
+
+app.put('/matricula/:id', async (req, resp) => {
+    try {
+            let { nome, chamada, curso, turma } = req.body;
+            let { id } = req.params;
+
+            let r = await db.tb_matricula.update(
+            {
+                nm_aluno: nome,
+                nr_chamada: chamada,
+                nm_curso: curso,
+                nm_turma: turma
+            },
+            {
+                where: { id_matricula: id }
+            }
+        )
+        resp.sendStatus(200);
+    } catch (error) {
+        resp.send({ erro: error.toString() })
+    }
+})
+
+app.delete('/matricula/:id', async (req, resp) => {
+    try {
+        let { id } = req.params;
+        let r = await db.tb_matricula.destroy({ where: { id_matricula: id } })
+        resp.sendStatus(200)
+    } catch (error) {
+        resp.send({ erro: error.toString() })
+    }
+})
+
+app.listen(process.env.PORT, x => console.log(`Server up at port ${process.env.PORT}`))
